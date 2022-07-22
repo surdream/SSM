574efad6cfcd79354eaaf25582875c4d3d9d8eb3
captdream <2548002489@qq.com>
2022-07-22 21:48:22 +0800

    动态SQL、日志配置、Druid连接池

--- MyBatis/dynamic_sql/src/main/java/com/dynamic_sql/dto/Member.java
+++ MyBatis/dynamic_sql/src/main/java/com/dynamic_sql/dto/Member.java
@@ -5,11 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Member {
+public class Member implements Serializable {
     private int memberId;
     private String memberNick;
     private String memberGender;
